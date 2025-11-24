5f04a4a8ffe3d8f5d63c76d5ad8f38502ee25697
PedroHenriqueFerreira <hpedro09062004@gmail.com>
2025-11-24 01:36:08 -0300

    up

--- main.cpp
+++ main.cpp
@@ -12,23 +12,17 @@ int main() {
 
     if (n == 1) {
         int x, y;
-
         cin >> x >> y;
-
         cout << 1 << "\n";
-
         return 0;
     }
     
     vector<pair<int, int>> pares(n);
-
     cin >> pares[0].first >> pares[0].second;
-
     int no = pares[0].second;
     int di = pares[0].first;
     int su = pares[0].second;
     int es = pares[0].first;
-
     for (int i = 1; i < n; i++) {
         cin >> pares[i].first >> pares[i].second;
 
@@ -44,6 +38,7 @@ int main() {
     pair<int, int> sul;
     pair<int, int> esq;
     pair<int, int> dir;
+
     bool has_norte = false;
     bool has_sul = false;
     bool has_esq = false;
@@ -63,11 +58,14 @@ int main() {
         int sul_dist = par.second - su;
         int dir_dist = di - par.first;
 
+        int min_esq_dir_dist = min(esq_dist, dir_dist);
+        int min_nor_sul_dist = min(norte_dist, sul_dist);
+
         if (no == par.second) {
             if (!has_norte) {
                 has_norte = true;
                 norte = par;
-            } else if (min(esq_dist, dir_dist) < min(abs(norte.first-es), abs(norte.first-di))) {
+            } else if (min_esq_dir_dist < min(abs(norte.first-es), abs(norte.first-di))) {
                 norte = par;
             }
         }
@@ -76,7 +74,7 @@ int main() {
             if (!has_sul) {
                 has_sul = true;
                 sul = par;
-            } else if (min(sul_dist, dir_dist) < min(abs(sul.first-es), abs(sul.first-di))) {
+            } else if (min_esq_dir_dist < min(abs(sul.first-es), abs(sul.first-di))) {
                 sul = par;
             }
         }
@@ -85,7 +83,7 @@ int main() {
             if (!has_esq) {
                 has_esq = true;
                 esq = par;
-            } else if (min(norte_dist, sul_dist) < min(abs(esq.second-no), abs(esq.second-su))) {
+            } else if (min_nor_sul_dist < min(abs(esq.second-no), abs(esq.second-su))) {
                 esq = par;
             }
         }
@@ -94,12 +92,12 @@ int main() {
             if (!has_dir) {
                 has_dir = true;
                 dir = par;
-            } else if (min(norte_dist, sul_dist) < min(abs(dir.second-no), abs(dir.second-su))) {
+            } else if (min_nor_sul_dist < min(abs(dir.second-no), abs(dir.second-su))) {
                 dir = par;
             }
         }
 
-        int curr_dist = max(max(norte_dist, sul_dist), max(esq_dist, dir_dist));
+        int curr_dist = max(min_nor_sul_dist, min_esq_dir_dist);
 
         if (!has_prox) {
             has_prox = true;
@@ -111,11 +109,11 @@ int main() {
             prox_dist = curr_dist;
         } else if (prox_dist == curr_dist) {
             if (largura < altura) {
-                if (min(abs(par.first - es), abs(par.first - di)) < min(abs(prox.first - es), abs(prox.first - di))) {
+                if (min_esq_dir_dist < min(abs(prox.first - es), abs(prox.first - di))) {
                     prox = par;
                 }
             } else {
-                if (min(abs(par.second - no), abs(par.second - su)) < min(abs(prox.second - no), abs(prox.second - su))) {
+                if (min_nor_sul_dist < min(abs(prox.second - no), abs(prox.second - su))) {
                     prox = par;
                 }
             }
@@ -125,11 +123,11 @@ int main() {
     if (dir == norte || dir == sul || esq == norte || esq == sul || norte == sul || esq == dir) {
         largura += k;
         altura += k;
-
         cout << largura * altura << "\n";
-
         return 0;
     } 
+
+    // -------------------------------------
 
     int norte_dist = min(abs(norte.first - es), abs(norte.first - di));
     int sul_dist = min(abs(sul.first - es), abs(sul.first - di));
@@ -151,32 +149,29 @@ int main() {
     }
 
     long long int largura_1 = largura, altura_1 = altura;
-    
     int norte_sul = min(norte_dist, sul_dist);
     int esq_dir = min(esq_dist, dir_dist);
-
     if (norte_sul < esq_dir) {
-        altura_1 += norte_sul;
-        altura_1 += k - norte_sul;
-        largura_1 += k - norte_sul;
+        altura_1 += min(k, norte_sul);
+        altura_1 += k - min(k, norte_sul);
+        largura_1 += k - min(k, norte_sul);
     } else if (norte_sul > esq_dir) {
-        largura_1 += esq_dir;
-        largura_1 += k - esq_dir;
-        altura_1 += k - esq_dir;
+        largura_1 += min(k, esq_dir);
+        largura_1 += k - min(k, esq_dir);
+        altura_1 += k - min(k, esq_dir);
     } else {
         if (largura_1 < altura_1) {
-            largura_1 += esq_dir;
-            largura_1 += k - esq_dir;
-            altura_1 += k - esq_dir;
+            largura_1 += min(k, esq_dir);
+            largura_1 += k - min(k, esq_dir);
+            altura_1 += k - min(k, esq_dir);
         } else {
-            altura_1 += norte_sul;
-            altura_1 += k - norte_sul;
-            largura_1 += k - norte_sul;
+            altura_1 += min(k, norte_sul);
+            altura_1 += k - min(k, norte_sul);
+            largura_1 += k - min(k, norte_sul);
         }
     }
 
     long long int largura_2 = largura, altura_2 = altura;
-
     for (int i = 0; i < k; i++) {
         if (largura_2 > altura_2) {
             altura_2++;
@@ -186,18 +181,16 @@ int main() {
     }
 
     long long int largura_3 = largura, altura_3 = altura;
-
-    if (prox == norte || prox == sul || prox == esq || prox == dir) {
-        norte_dist = no - prox.second;
-        esq_dist = prox.first - es;
-        sul_dist = prox.second - su;
-        dir_dist = di - prox.first;
+    if (prox != norte && prox != sul && prox != esq && prox != dir && prox_dist < k) {
+        norte_dist = min(k, no - prox.second);
+        esq_dist = min(k, prox.first - es);
+        sul_dist = min(k, prox.second - su);
+        dir_dist = min(k, di - prox.first);
 
         norte_sul = min(norte_dist, sul_dist);
         esq_dir = min(esq_dist, dir_dist);
 
         k = k - min(norte_sul, esq_dir);
-
         if (norte_sul < esq_dir) {
             esq_dir -= norte_sul;
             norte_sul = 0;
@@ -207,23 +200,16 @@ int main() {
         }
 
         if (norte_sul < esq_dir) {
-            altura_3 += norte_sul;
-            altura_3 += k - norte_sul;
-            largura_3 += k - norte_sul;
-        } else if (norte_sul > esq_dir) {
-            largura_3 += esq_dir;
-            largura_3 += k - esq_dir;
+            altura_3 += esq_dir;
             altura_3 += k - esq_dir;
+            largura_3 += k - esq_dir;
+        } else if (norte_sul > esq_dir) {
+            largura_3 += norte_sul;
+            largura_3 += k - norte_sul;
+            altura_3 += k - norte_sul;
         } else {
-            if (largura_3 < altura_3) {
-                largura_3 += esq_dir;
-                largura_3 += k - esq_dir;
-                altura_3 += k - esq_dir;
-            } else {
-                altura_3 += norte_sul;
-                altura_3 += k - norte_sul;
-                largura_3 += k - norte_sul;
-            }
+            largura_3 += k;
+            altura_3 += k;
         }
     }
 
